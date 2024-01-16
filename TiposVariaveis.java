d1e22b7060dfbb4bd475d04df8640decc989ec80
MaicondeMariaDuart <156396043+MaicondeMariaDuart@users.noreply.github.com>
2024-01-16 01:18:56 -0300

    Add files via upload

---
+++ TiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        final double VALOR_DE_PI = 3.14;
+
+       
+
+
+        
+    }
+}
